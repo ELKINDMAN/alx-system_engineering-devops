f241ad38fd8e3dd3dbcc8ed1b12cc632a8fa4936
elkindman <elkanahkindness@gmail.com>
2024-08-18 17:01:15 +0100

    task 1

---
+++ 0x15-api/1-export_to_csv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Returns Information About A page"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = int(sys.argv[1])
+
+    api_url_user_name = f"https://jsonplaceholder.typicode.com/users/{id}"
+    user_name_res = requests.get(api_url_user_name).json()
+
+    api_url_todos = f"{api_url_user_name}/todos"
+
+    user_todo = requests.get(api_url_todos).json()
+
+    usr_name = user_name_res.get('username')
+
+    api_data = f"{id}.csv"
+    with open(api_data, mode="w", newline="") as file:
+        for task in user_todo:
+            file.write('"{}","{}","{}","{}"\n'.format(
+                id, usr_name, task.get('completed'), task.get('title')))
